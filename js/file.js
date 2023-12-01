f44a64fb42ee2294d4b608fab16654d69951142c
austin-tw <148500378+austin-tw@users.noreply.github.com>
2023-12-01 15:10:16 -0700

    js v1

---
+++ js/file.js
@@ -0,0 +1,31 @@
+let x = new Date().getFullYear();
+document.getElementById("year").innerHTML = x;
+
+document.getElementById("btn-alert").addEventListener("click", function() {
+    alert("I can't believe someone still fell for this type of clickbait in " +x +"...");
+  });
+
+
+  document.getElementById('btn-alert').onmouseover = () => {
+    document.getElementById('btn-alert').innerText = 'Claim Your $100 Prize Now!';
+};
+
+document.getElementById('btn-alert').onmouseleave = () => {
+    document.getElementById('btn-alert').innerText = 'Click Here to Win a $100 Amazon Giftcard!';
+};
+
+
+let count = 1;
+document.getElementById('btn-counter').onclick = () => {
+     count = count + 1;
+     document.getElementById('txt-counter').innerHTML = 'Number: ' +count
+
+     document.getElementById('txt-counter').classList.remove("even", "odd");
+
+     if (count % 2 == 0) {
+    document.getElementById('txt-counter').classList.add('even');
+} else {
+    document.getElementById('txt-counter').classList.add('odd'); 
+}
+}
+
